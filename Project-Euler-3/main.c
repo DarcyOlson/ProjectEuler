fb48093e36643059d5d49d80969497d50f2047bb
DarcyOlson <darcy.olson94@gmail.com>
2017-06-17 17:20:51 -0700

    initial function declarations for problem

---
+++ Project-Euler-3/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+    This code solves the following problem:
+
+    The prime factors of 13195 are 5, 7, 13 and 29.
+
+    What is the largest prime factor of the number 600851475143 ?
+*/
+int largest_prime_factor(int n);//This function will find the largest prime factor of a given input
+void generate_primes(int primes[]);
+int find_max_prime(int n);
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+int generate_primes()
