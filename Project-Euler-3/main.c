3dfef33ef5eef1ee554023362db96d9aa5d0f133
DarcyOlson <darcy.olson94@gmail.com>
2017-06-18 22:31:55 -0700

    functional solution to problem 3

--- Project-Euler-3/main.c
+++ Project-Euler-3/main.c
@@ -8,18 +8,30 @@
 
     What is the largest prime factor of the number 600851475143 ?
 */
-int largest_prime_factor(int n);//This function will find the largest prime factor of a given input
+int largest_prime_factor(double n);//This function will find the largest prime factor of a given input
 void sieve_of_erastothenes(int input[], int size);//This function will find all prime numbers less than size
 void initialize_sieve_array(int input[], int size);
 int find_largest_prime_factor(int input[], int size);
 
 int main()
 {
-    //int size = (int)ceil(sqrt(600851475143));
-    //int test[size];
-    //sieve_of_erastothenes(test, size);
+    double largest = 0.0;
+    int size = (int)ceil(sqrt(600851475143));
     double big_num = 600851475143.0;
-    printf("%f", big_num);
+    int test[size];
+    sieve_of_erastothenes(test, size);
+    int i;
+    for(i = 0; i < size; i++){
+        if(test[i] != 0){
+            printf("testing %d \n",i);
+            double test = big_num/i;
+            if((test - floor(test)) == 0){
+                largest = i;
+            }
+        }
+    }
+    printf("%f",largest);
+    //*/
     return 0;
 }
 
@@ -34,14 +46,10 @@ void sieve_of_erastothenes(int input[], int size){
             }
         }
     }
-    int j;
-    for(j = 0; j < size; j++){
-        printf("%d", input[j]);
-    }
 }
 
-int largest_prime_factor(int n){
-    int factor_upper_bound = n;
+int largest_prime_factor(double n){
+    int factor_upper_bound = (int)ceil(sqrt(n));
     int primes[factor_upper_bound];
     initialize_sieve_array(primes,factor_upper_bound);
     sieve_of_erastothenes(primes,factor_upper_bound);
@@ -59,10 +67,14 @@ void initialize_sieve_array(int input[], int size){
 int find_largest_prime_factor(int input[], int size){
     int i, largest_factor = 0;
     for(i = 0;i < size; i++){
+        printf("We evaluate %d:",i);
         if(input[i] == 1 && size%i == 0){
+            printf("find it satisfactory \n");
             if(i > largest_factor){
                 largest_factor = i;
             }
+        }else{
+            printf("We find it unsatisfactory \n");
         }
     }
     return largest_factor;
